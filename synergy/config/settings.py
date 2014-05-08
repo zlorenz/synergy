d465dd8829789a1a2b84b7cc81416ff9b027b024
zlorenz <zglorenz@gmail.com>
2014-05-08 15:28:12 -0700

    Adds content to Risk Management block, deletes Asset Protection column

--- synergy/config/settings.py
+++ synergy/config/settings.py
@@ -134,7 +134,7 @@ class Common(Configuration):
 
     ########## GENERAL CONFIGURATION
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#time-zone
-    TIME_ZONE = 'America/Los_Angeles'
+    TIME_ZONE = 'America/Phoenix'
 
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#language-code
     LANGUAGE_CODE = 'en-us'
