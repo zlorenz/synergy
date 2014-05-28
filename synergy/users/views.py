627ff7a40f22297f86c869d05d4a2c8ba7ba699a
Will Farley <will@django.nu>
2014-05-28 16:18:53 -0700

    adds contact form logic

--- synergy/users/views.py
+++ synergy/users/views.py
@@ -17,6 +17,41 @@ from .forms import UserForm
 # Import the customized User model
 from .models import User
 
+from django.shortcuts import render, render_to_response
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.core.mail import mail_admins
+from django.contrib import messages
+from django.template import RequestContext
+from django.http import HttpResponseRedirect, Http404, HttpResponse
+
+
+def contact(request):
+    if request.POST:
+        post = request.POST
+        first_name = post.get('first_name')
+        last_name = post.get('last_name')
+        phone = post.get('phone')
+        email = post.get('email')
+        learn = post.get('learn')
+        topic = post.get('topic')
+        _message = post.get('message')
+        message = 'first_name: {}, last_name: {}, phone: {} email: {}, learn: {}, topic: {}, message: {}'.format(
+            first_name, last_name, phone, email, learn, topic, _message)
+        subject = unicode('Submission from {} {}').format(first_name, last_name)
+        mail_admins(subject, message)
+        _next = '/'
+        messages.success(request, 'Thanks for the submission!')
+        if _next:
+            return HttpResponseRedirect(_next)
+
+    _next = ""
+    if request.GET.get('next'):
+        _next = request.GET.get('next')
+
+    context = {'next': _next}
+    return render_to_response('pages/home.html',
+        context,
+        context_instance=RequestContext(request))
 
 class UserDetailView(LoginRequiredMixin, DetailView):
     model = User
