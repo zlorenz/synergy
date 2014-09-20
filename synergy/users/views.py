0710e8a33700f6886f55a3cf63f9cf0a5f234aab
goldhand <will@django.nu>
2014-09-19 18:44:16 -0700

    adds refferal email shit

--- synergy/users/views.py
+++ synergy/users/views.py
@@ -19,12 +19,15 @@ from .models import User
 
 from django.shortcuts import render, render_to_response
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
-from django.core.mail import mail_admins
+from django.core.mail import mail_admins, send_mail
 from django.contrib import messages
+from django.contrib.sites.models import Site
+from django.conf import settings
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, Http404, HttpResponse
 
 
+@csrf_protect
 def contact(request):
     if request.POST:
         post = request.POST
@@ -50,6 +53,46 @@ def contact(request):
 
     context = {'next': _next}
     return render_to_response('pages/home.html',
+        context,
+        context_instance=RequestContext(request))
+
+
+@csrf_protect
+def refer(request):
+    if request.POST:
+        post = request.POST
+        name = post.get('name', u'')
+        email = post.get('email', u'')
+        referral_name = post.get('referral_name', u'')
+        phone = post.get('referral_phone', u'')
+        referral_email = post.get('referral_email', u'')
+        _message = post.get('message', u'')
+        message = 'name: {}, email: {}, referral_name: {}, referral_phone: {}, \
+        referral_email: {}, message: {}'.format(
+            name, email, referral_name, phone, referral_email, _message)
+        subject = unicode('Referral from {} for {}').format(name, referral_name)
+        mail_admins(subject, message)
+        _next = '/'
+        messages.success(request, 'Thanks for the submission!')
+
+        current_site = Site.objects.get(id=settings.SITE_ID)
+
+        referral_email_text = '<p>Hello {},</p><p> {}, <br> Please click on this \
+        link for more information:<br><a href="{}">{}</a><br>Thank you,<br> \
+        {}</p>'.format(referral_name, _message, current_site.domain, 
+                       current_site.name, name)
+        send_mail("Check This Out", referral_email_text, email,
+            [referral_email], fail_silently=True)
+
+        if _next:
+            return HttpResponseRedirect(_next)
+
+    _next = ""
+    if request.GET.get('next'):
+        _next = request.GET.get('next')
+
+    context = {'next': _next}
+    return render_to_response('pages/clients.html',
         context,
         context_instance=RequestContext(request))
 
